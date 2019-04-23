49cdde79eda3561786eb3beb387df8ea7ffedb2f
Digvijay Upadhyay <digvijay.upadhyay@globant.com>
2019-04-23 14:40:40 +0100

    state persistance

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import AddressForm from './components/AddressForm';
 import configureStore from './store';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <AddressForm />
+        <PersistGate loading={null} persistor={persistor}>
+          <AddressForm />
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import thunkMiddleware from 'redux-thunk';
+import storage from 'redux-persist/lib/storage';
 
 import reducer from './restaurants/reducers';
+
+const initialState = {};
 
 const rootReducer = combineReducers({
   restaurants: reducer
 });
 
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore() {
   const middlewares = [thunkMiddleware];
-  const store = createStore(rootReducer, applyMiddleware(...middlewares));
-  return store;
+  const store = createStore(
+    persistedReducer,
+    initialState,
+    applyMiddleware(...middlewares)
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
 }
